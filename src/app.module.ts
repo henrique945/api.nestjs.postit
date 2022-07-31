8c053128c2e888e5a99a1b6322d8472c0ebc9fb5
Guilherme Chaguri <guichaguri@gmail.com>
2022-07-31 12:13:48 -0300

    feat(database): switch to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,22 @@ import { AuthModule } from './modules/auth/auth.module';
 import { NoteCommentModule } from './modules/note-comment/note-comment.module';
 import { NoteLikeModule } from './modules/note-like/note-like.module';
 import { NoteRoutingModule } from './modules/note/note-routing.module';
-import { NoteModule } from './modules/note/note.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'test.db',
+      type: 'postgres',
+      url: enviroment.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
       logging: enviroment.DATABASE_LOGGING === 'true',
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        }
+      },
     }),
     UserModule,
     AuthModule,
